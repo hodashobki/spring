96a7d8d977e75a2702c0774bf72b076012373297
Hoda Shobaki <shobkihoda@gmail.com>
2021-06-19 13:16:26 +0300

    updated 19/06/2021

---
+++ ninjagold/src/main/java/com/Hoda/ninjagold/GoldController.java
@@ -0,0 +1,77 @@
+package com.Hoda.ninjagold;
+
+import javax.servlet.http.HttpSession;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GoldController {
+@RequestMapping("")
+public String gold(HttpSession session,Model model) {
+	ArrayList<String> activity=new ArrayList<String>();
+	if (session.getAttribute("counter")==null){
+		session.setAttribute("counter", 0);	
+	}
+	if (session.getAttribute("activity")==null){
+		session.setAttribute("activity",activity );	
+	}
+//	it worked without this:
+	model.addAttribute("counter",session.getAttribute("counter"));
+	model.addAttribute("activity",session.getAttribute("activity"));
+//	else 
+	return "gold.jsp";
+}
+@RequestMapping(value="process_money", method=RequestMethod.POST)
+public String proceesMony(@RequestParam(value="gold") String gold,HttpSession session,Model model) {
+	Random rand = new Random();
+	int rangold = 0;
+	int goold=(int)session.getAttribute("counter");
+//	if(RequestMethod.equals"POST")
+	//	ArrayList<String> activity=new ArrayList<String>;
+	LocalDateTime currenttime=LocalDateTime .now();
+	SimpleDateFormat formatter = new SimpleDateFormat("dd/MMMM/yyyy HH:mm:ss");  
+	ArrayList<String> activity=(ArrayList<String>)session.getAttribute("activity");
+	if(gold.equals("farm")) {
+		rangold=rand.nextInt((20-10)+1)+10;
+		activity.add(String.format("You entered a farm and earned %d gold",rangold));
+	}
+	else if(gold.equals("cave")) {
+		rangold=rand.nextInt((10-5)+1)+5;
+		activity.add(String.format("You entered a cave and earned %d gold \n",rangold));
+	}
+	else if(gold.equals("house")) {
+		rangold=rand.nextInt((5-2)+1)+2;
+		activity.add(String.format("You entered a house and earned %d gold \n",rangold));
+	}
+	else if(gold.equals("casino")) {
+		rangold=rand.nextInt(50+50)+1-50;
+		if(rangold>0) {
+			activity.add(String.format("You entered a casino and earned %d gold \n",rangold));
+		}
+		else {
+			activity.add(String.format("<span style='color:red;'>You entered a casino and lost %d gold..Outch</span>",rangold));
+		}
+	}
+	int totalgold=rangold+=goold;
+	session.setAttribute("counter", totalgold);
+	session.setAttribute("activity",activity );
+	
+	
+	return "redirect:/";
+	
+}
+@RequestMapping("/reset")
+public String logout(HttpSession session) {
+	
+		session.invalidate();
+	return "redirect:/";
+}
+}
---
+++ ninjagold/src/main/webapp/WEB-INF/gold.jsp
@@ -0,0 +1,64 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Ninja Gold</title>
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>your gold <c:out value="${counter}"/></h1>
+    <main>
+        <div>
+            <h2>Farm</h2>
+            <p>Earnes 10-20 gold</p>
+            <form action="/process_money" method="POST">
+                <input type="hidden" name="gold" value="farm">
+                <input type="submit" value="Find Gold">
+
+            </form>
+        </div>
+        <div><h2>Cave</h2>
+            <p>Earnes 5-10gold</p>
+            <form action="/process_money" method="POST">
+                <input type="hidden" name="gold" value="cave">
+                <input type="submit" value="Find Gold" >
+
+            </form></div>
+        <div><h2>House</h2>
+            <p>Earnes 2-5 gold</p>
+            <form action="/process_money" method="POST">
+                <input type="hidden" name="gold" value="house">
+                <input type="submit" value="Find Gold">
+
+            </form></div>
+        <div><h2>Casino</h2>
+            <p>Earnes/Loses 0-50 gold</p>
+            <form action="/process_money" method="POST">
+                <input type="hidden" name="gold" value="casino">
+                <input type="submit" value="Find Gold">
+
+            </form>
+        </div>
+    </main>
+    <center>
+    <div>
+        <form action="/reset" method="POST">
+            <input type="submit" value="Play again">
+        </form>
+    </div>
+    <h3>Activity</h3>
+    <div>
+    <c:forEach items="${activity}" var="activity">
+    <p><c:out value="${activity}"/></p>
+    </c:forEach>
+    </div>
+    
+    
+</center>
+    
+</body>
+</html>
